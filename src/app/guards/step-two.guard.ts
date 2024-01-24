bf7152083159a223a0b2f67664e3c937d6ac309c
Martin Ferret <firdaouszm@gmail.com>
2024-01-24 09:51:12 +0100

    import unused + null operator suppressed from template.

--- src/app/guards/step-two.guard.ts
+++ src/app/guards/step-two.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivateFn } from '@angular/router';
 import {inject} from "@angular/core";
 import {SharedService} from "../services/shared.service";
-import {map, Observable} from "rxjs";
+import {map} from "rxjs";
 
 export const stepTwoGuard: CanActivateFn = () => {
   return inject(SharedService).getSelectedTesla().pipe(
